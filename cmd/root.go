90519e6deabd599f9d8a4805f647131d0dd78e49
Xin <xin@imfing.com>
2023-04-01 23:11:03 +0100

    feat: add persistent flag `open-api-key`
    can also be set via env var and read by viper

--- cmd/root.go
+++ cmd/root.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -22,8 +23,13 @@ func Execute() {
 
 func init() {
 	cobra.OnInitialize(initConfig)
+
+	rootCmd.PersistentFlags().String("openai-api-key", "", "OpenAI API Key")
 }
 
 func initConfig() {
 	viper.AutomaticEnv()
+	viper.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
+
+	viper.BindPFlags(rootCmd.PersistentFlags())
 }
