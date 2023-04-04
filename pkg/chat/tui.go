7d927ba8d14ec3665d9831b5f0d80b09d6288ba0
Xin <xin@imfing.com>
2023-04-04 21:56:12 +0100

    feat: add help view

--- pkg/chat/tui.go
+++ pkg/chat/tui.go
@@ -2,13 +2,14 @@ package chat
 
 import (
 	"fmt"
+	"github.com/charmbracelet/bubbles/help"
+	"github.com/charmbracelet/bubbles/key"
 	"github.com/charmbracelet/bubbles/spinner"
 	"github.com/charmbracelet/bubbles/textarea"
 	"github.com/charmbracelet/bubbles/viewport"
 	"github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/glamour"
 	"github.com/charmbracelet/lipgloss"
-	"github.com/muesli/termenv"
 	"github.com/spf13/viper"
 	"strings"
 )
@@ -29,6 +30,39 @@ var (
 
 var textAreaHeight = 4
 
+type keymap struct {
+	Esc, Quit, Send key.Binding
+}
+
+var keys = keymap{
+	Esc: key.NewBinding(
+		key.WithKeys("esc"),
+		key.WithHelp("esc", "exit fullscreen"),
+	),
+	Send: key.NewBinding(
+		key.WithKeys("ctrl+s"),
+		key.WithHelp("ctrl+s", "send"),
+	),
+	Quit: key.NewBinding(
+		key.WithKeys("ctrl+c"),
+		key.WithHelp("ctrl+c", "quit"),
+	),
+}
+
+// ShortHelp returns keybindings to be shown in the mini help view. It's part
+// of the key.Map interface.
+func (k keymap) ShortHelp() []key.Binding {
+	return []key.Binding{k.Send, k.Esc, k.Quit}
+}
+
+// FullHelp returns keybindings for the expanded help view. It's part of the
+// key.Map interface.
+func (k keymap) FullHelp() [][]key.Binding {
+	return [][]key.Binding{
+		{k.Send, k.Esc, k.Quit},
+	}
+}
+
 type Model struct {
 	viewport viewport.Model
 	messages []string
@@ -37,6 +71,8 @@ type Model struct {
 	renderer *glamour.TermRenderer
 	err      error
 	waiting  bool
+	keys     keymap
+	help     help.Model
 }
 
 func (m Model) Init() tea.Cmd {
@@ -58,12 +94,12 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
-		switch msg.Type {
-		case tea.KeyEsc:
+		switch {
+		case key.Matches(msg, m.keys.Esc):
 			return m, tea.ExitAltScreen
-		case tea.KeyCtrlC:
+		case key.Matches(msg, m.keys.Quit):
 			return m, tea.Quit
-		case tea.KeyEnter:
+		case key.Matches(msg, m.keys.Send):
 			input, _ := m.renderer.Render(m.textarea.Value())
 			m.messages = append(m.messages, senderStyle.Render("You: ")+input)
 			m.viewport.SetContent(strings.Join(m.messages, "\n"))
@@ -79,14 +115,11 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		m.viewport.Height = msg.Height - (8 + textAreaHeight)
 		m.textarea.SetWidth(msg.Width - x)
 
-		glamourStyle := LightStyleConfig
-		if termenv.HasDarkBackground() {
-			glamourStyle = DarkStyleConfig
-		}
 		m.renderer, _ = glamour.NewTermRenderer(
-			glamour.WithStyles(glamourStyle),
-			glamour.WithWordWrap(msg.Width-x),
+			glamour.WithAutoStyle(),
+			glamour.WithWordWrap(msg.Width-x-2),
 		)
+		// TODO: re-render messages based on new word wrap width
 
 	case spinner.TickMsg:
 		var cmd tea.Cmd
@@ -124,7 +157,7 @@ func (m Model) View() string {
 		s += m.spinner.View() + " sending...\n\n"
 	}
 
-	s += helpStyle.Render("(ctrl+c to quit)")
+	s += m.help.View(m.keys)
 
 	return appStyle.Render(s)
 }
@@ -152,9 +185,9 @@ func NewModel() Model {
 	vp := viewport.New(50, 10)
 	vp.SetContent(fmt.Sprintf(
 		"%s\n\n%s\n%s",
-		"Welcome to use gptui Chat",
+		"Welcome to use ChatGPT terminal UI",
 		helpStyle.Render("Model: "+chatModel),
-		"Type a message and press Enter to send."))
+		"Type a message and press Ctrl+S to send."))
 
 	s := spinner.New()
 	s.Style = spinnerStyle
@@ -166,5 +199,7 @@ func NewModel() Model {
 		err:      nil,
 		waiting:  false,
 		spinner:  s,
+		help:     help.New(),
+		keys:     keys,
 	}
 }
