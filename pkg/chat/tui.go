1ed84253e24a1e426bf7b54a49c05586d38635d4
Xin <xin@imfing.com>
2023-04-05 22:17:22 +0100

    feat: add multiline mode

--- pkg/chat/tui.go
+++ pkg/chat/tui.go
@@ -21,17 +21,21 @@ var (
 	chatStyle     = lipgloss.NewStyle().Background(lipgloss.Color("36")).Foreground(lipgloss.Color("#FAFAFA")).Padding(0, 1)
 	textAreaStyle = lipgloss.NewStyle().Border(lipgloss.RoundedBorder()).BorderForeground(lipgloss.Color("238")).Padding(0, 1)
 	spinnerStyle  = lipgloss.NewStyle().Foreground(lipgloss.Color("63")).MarginTop(4)
-	helpStyle     = lipgloss.NewStyle().Foreground(lipgloss.Color("241")).Margin(1, 0)
+	helpStyle     = lipgloss.NewStyle().Foreground(lipgloss.Color("241"))
 	errorStyle    = lipgloss.NewStyle().Foreground(lipgloss.Color("9"))
 )
 
 var textAreaHeight = 4
 
 type keymap struct {
-	Esc, Quit, Send, NewLine key.Binding
+	Help, Esc, Quit, Send, Multiline key.Binding
 }
 
 var keys = keymap{
+	Help: key.NewBinding(
+		key.WithKeys("ctrl+h"),
+		key.WithHelp("ctrl+h", "help"),
+	),
 	Esc: key.NewBinding(
 		key.WithKeys("esc"),
 		key.WithHelp("esc", "exit fullscreen"),
@@ -40,9 +44,9 @@ var keys = keymap{
 		key.WithKeys("enter"),
 		key.WithHelp("enter", "send"),
 	),
-	NewLine: key.NewBinding(
-		key.WithKeys("ctrl+n"),
-		key.WithHelp("ctrl+n", "newline"),
+	Multiline: key.NewBinding(
+		key.WithKeys("ctrl+l"),
+		key.WithHelp("ctrl+l", "toggle multi-line"),
 	),
 	Quit: key.NewBinding(
 		key.WithKeys("ctrl+c"),
@@ -53,29 +57,33 @@ var keys = keymap{
 // ShortHelp returns keybindings to be shown in the mini help view. It's part
 // of the key.Map interface.
 func (k keymap) ShortHelp() []key.Binding {
-	return []key.Binding{k.Send, k.NewLine, k.Esc, k.Quit}
+	return []key.Binding{k.Help, k.Send, k.Quit}
 }
 
 // FullHelp returns keybindings for the expanded help view. It's part of the
 // key.Map interface.
 func (k keymap) FullHelp() [][]key.Binding {
 	return [][]key.Binding{
-		{k.Send, k.NewLine, k.Esc, k.Quit},
+		{k.Help, k.Send, k.Quit},
+		{k.Multiline, k.Esc},
 	}
 }
 
 type Model struct {
-	viewport viewport.Model
-	messages []string
-	textarea textarea.Model
-	spinner  spinner.Model
-	renderer *glamour.TermRenderer
-	err      error
-	waiting  bool
-	keys     keymap
-	sub      chan CompletionStreamResponse
-	stream   string
-	help     help.Model
+	viewport  viewport.Model
+	messages  []string
+	textarea  textarea.Model
+	multiline bool
+	spinner   spinner.Model
+	renderer  *glamour.TermRenderer
+	waiting   bool
+	keys      keymap
+	sub       chan CompletionStreamResponse
+	stream    string
+	width     int
+	height    int
+	err       error
+	help      help.Model
 }
 
 func (m Model) Init() tea.Cmd {
@@ -83,7 +91,6 @@ func (m Model) Init() tea.Cmd {
 		textarea.Blink,
 		tea.EnterAltScreen,
 		m.spinner.Tick,
-		waitForStreamResponse(m.sub),
 	)
 }
 
@@ -105,29 +112,41 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch {
+		case key.Matches(msg, m.keys.Help):
+			// toggle help
+			m.help.ShowAll = !m.help.ShowAll
 		case key.Matches(msg, m.keys.Esc):
 			return m, tea.ExitAltScreen
 		case key.Matches(msg, m.keys.Quit):
 			return m, tea.Quit
+		case key.Matches(msg, m.keys.Multiline):
+			// toggle multiline
+			m.multiline = !m.multiline
+			m.textarea.ShowLineNumbers = m.multiline
+			// refresh textarea width
+			m.textarea.SetWidth(m.width - appStyle.GetHorizontalFrameSize())
 		case key.Matches(msg, m.keys.Send):
-			input, _ := m.renderer.Render(m.textarea.Value())
-			m.messages = append(m.messages, senderStyle.Render("You")+"\n"+input)
-			m.viewport.SetContent(strings.Join(m.messages, "\n"))
+			if !m.multiline && !m.waiting {
+				input, _ := m.renderer.Render(m.textarea.Value())
+				m.messages = append(m.messages, senderStyle.Render("You")+"\n"+input)
+				m.viewport.SetContent(strings.Join(m.messages, "\n"))
 
-			commands = append(commands,
-				waitForStreamResponse(m.sub),
-				sendChatCompletionStreamRequest(endpoint, token, chatModel, m.textarea.Value(), m.sub))
+				commands = append(commands,
+					waitForStreamResponse(m.sub),
+					sendChatCompletionStreamRequest(endpoint, token, chatModel, m.textarea.Value(), m.sub))
 
-			m.textarea.Reset()
-			m.viewport.GotoBottom()
-			m.waiting = true
+				m.textarea.Reset()
+				m.viewport.GotoBottom()
+				m.waiting = true
+			}
 		}
 
 	case tea.WindowSizeMsg:
-		x, _ := appStyle.GetFrameSize()
-		m.viewport.Width = msg.Width - x
+		m.width, m.height = msg.Width, msg.Height
+		h := appStyle.GetHorizontalFrameSize()
+		m.viewport.Width = msg.Width - h
 		m.viewport.Height = msg.Height - (8 + textAreaHeight)
-		m.textarea.SetWidth(msg.Width - x)
+		m.textarea.SetWidth(msg.Width - h)
 
 		glamourStyle := LightStyleConfig
 		if termenv.HasDarkBackground() {
@@ -135,7 +154,7 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		}
 		m.renderer, _ = glamour.NewTermRenderer(
 			glamour.WithStyles(glamourStyle),
-			glamour.WithWordWrap(msg.Width-x-2),
+			glamour.WithWordWrap(msg.Width-h-2),
 		)
 		// TODO: re-render messages based on new word wrap width
 
@@ -203,10 +222,12 @@ func newTextArea() textarea.Model {
 	t := textarea.New()
 	t.Prompt = ""
 	t.Placeholder = "Send a message..."
-	t.CharLimit = 0
+	t.CharLimit = -1
 	t.FocusedStyle.CursorLine = lipgloss.NewStyle()
+	t.FocusedStyle.EndOfBuffer = helpStyle
 	t.FocusedStyle.Base = textAreaStyle
 	t.ShowLineNumbers = false
+	t.KeyMap.DeleteCharacterBackward = key.NewBinding(key.WithKeys("backspace"))
 	t.Blur()
 	return t
 }
@@ -224,21 +245,22 @@ func NewModel() Model {
 	vp.SetContent(fmt.Sprintf(
 		"%s\n\n%s\n%s",
 		"Welcome to use ChatGPT terminal UI",
-		helpStyle.Render("Model: "+chatModel),
+		helpStyle.Render("Model: "+chatModel+"\n"),
 		"Type a message and press Ctrl+S to send."))
 
 	s := spinner.New()
 	s.Style = spinnerStyle
 
 	return Model{
-		textarea: ta,
-		messages: []string{},
-		viewport: vp,
-		err:      nil,
-		waiting:  false,
-		spinner:  s,
-		help:     help.New(),
-		keys:     keys,
-		sub:      make(chan CompletionStreamResponse),
+		textarea:  ta,
+		viewport:  vp,
+		spinner:   s,
+		help:      help.New(),
+		keys:      keys,
+		messages:  []string{},
+		multiline: false,
+		waiting:   false,
+		sub:       make(chan CompletionStreamResponse),
+		err:       nil,
 	}
 }
